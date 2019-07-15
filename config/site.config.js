b51a498652068141a7a8de40198155893d6ada78
Michael Litschauer <michael.litschauer@ebcont.com>
2019-07-15 14:43:44 +0200

    fixed build errors

--- config/site.config.js
+++ config/site.config.js
@@ -15,7 +15,7 @@ const config = {
   site_description: '',
 
   // Your website's URL, used for sitemap
-  site_url: '',
+  site_url: 'https://gendercomics.net',
 
   // Google Analytics tracking ID (leave blank to disable)
   googleAnalyticsUA: '',
