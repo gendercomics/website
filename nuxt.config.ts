39dec745ffa0c83cb0656c0eed14eba9c03196a6
michael litschauer <michael.litschauer@gmail.com>
2024-03-26 16:53:04 +0100

    removed assets dir config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,6 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   pages: true,
   modules: ['@nuxt/content'],
-  dir: {
-    assets: 'assets',
-  },
   css: [
     'assets/css/fonts.css',
     'assets/css/design.css',
