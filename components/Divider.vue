273c4da1f7fc7799d90432eb114afa90d5c0c0a1
michael litschauer <michael.litschauer@gmail.com>
2024-03-27 22:24:43 +0100

    divider

--- components/Divider.vue
+++ components/Divider.vue
@@ -2,222 +2,229 @@
 const props = defineProps({
   t1: {
     type: Boolean,
-    default: false
+    default: false,
   },
   t2: {
     type: Boolean,
-    default: false
+    default: false,
   },
   t3: {
     type: Boolean,
-    default: false
+    default: false,
   },
   t3flat: {
     type: Boolean,
-    default: false
+    default: false,
   },
   t4: {
     type: Boolean,
-    default: false
+    default: false,
   },
   t4flat: {
     type: Boolean,
-    default: false
+    default: false,
   },
   t5: {
     type: Boolean,
-    default: false
+    default: false,
   },
   t6: {
     type: Boolean,
-    default: false
+    default: false,
   },
 
   b1: {
     type: Boolean,
-    default: false
+    default: false,
   },
   b2: {
     type: Boolean,
-    default: false
+    default: false,
   },
   b3: {
     type: Boolean,
-    default: false
+    default: false,
   },
   b3flat: {
     type: Boolean,
-    default: false
+    default: false,
   },
   b4: {
     type: Boolean,
-    default: false
+    default: false,
   },
   b4flat: {
     type: Boolean,
-    default: false
+    default: false,
   },
   b5: {
     type: Boolean,
-    default: false
+    default: false,
   },
   b6: {
     type: Boolean,
-    default: false
-  }
+    default: false,
+  },
 })
-
 </script>
 
 <template>
-  <div class='container'>
-    <div id='divider-top-row'>
-      <div v-if='t1' id='divider-top-1' />
-      <div v-else id='divider-corner-invisible' />
-      <div v-if='t2' id='divider-top-2' />
-      <div v-else class='invisible' />
-      <div v-if='t3' id='divider-top-3' />
-      <div v-else-if='t3flat' id='divider-top-3-flat' />
-      <div v-else id='divider-corner-invisible' />
-      <div v-if='t4' id='divider-top-4' />
-      <div v-else-if='t4flat' id='divider-top-4-flat' />
-      <div v-else id='divider-corner-invisible' />
-      <div v-if='t5' id='divider-top-5' />
-      <div v-else class='invisible' />
-      <div v-if='t6' id='divider-top-6' />
-      <div v-else id='divider-corner-invisible' />
+  <div class="container">
+    <!-- top row -->
+    <div class="fixed-width">
+      <div v-if="t1" class="divider-top-1" />
     </div>
-    <div id='divider-bottom-row'>
-      <div v-if='b1' id='divider-bottom-1' />
-      <div v-else id='divider-corner-invisible' />
-      <div v-if='b2' id='divider-bottom-2' />
-      <div v-else class='invisible' />
-      <div v-if='b3' id='divider-bottom-3' />
-      <div v-else-if='b3flat' id='divider-bottom-3-flat' />
-      <div v-else id='divider-corner-invisible' />
-      <div v-if='b4' id='divider-bottom-4' />
-      <div v-else-if='b4flat' id='divider-bottom-4-flat' />
-      <div v-else id='divider-corner-invisible' />
-      <div v-if='b5' id='divider-bottom-5' />
-      <div v-else class='invisible' />
-      <div v-if='b6' id='divider-bottom-6' />
-      <div v-else id='divider-corner-invisible' />
+    <div>
+      <div v-if="t2" class="divider-top-2" />
+      <div v-else class="invisible" />
+    </div>
+    <div class="fixed-width">
+      <div v-if="t3" class="divider-top-3" />
+      <div v-else-if="t3flat" class="divider-top-3-flat" />
+    </div>
+    <div class="fixed-width">
+      <div v-if="t4" class="divider-top-4" />
+      <div v-else-if="t4flat" class="divider-top-4-flat" />
+    </div>
+    <div>
+      <div v-if="t5" class="divider-top-5" />
+      <div v-else class="invisible" />
+    </div>
+    <div class="fixed-width">
+      <div v-if="t6" class="divider-top-6" />
+    </div>
+
+    <!-- bottom row -->
+    <div class="fixed-width mt--2">
+      <div v-if="b1" class="divider-bottom-1" />
+    </div>
+    <div class="mt--2">
+      <div v-if="b2" class="divider-bottom-2" />
+      <div v-else class="invisible" />
+    </div>
+    <div class="fixed-width mt--2">
+      <div v-if="b3" class="divider-bottom-3" />
+      <div v-else-if="b3flat" class="divider-bottom-3-flat" />
+    </div>
+    <div class="fixed-width mt--2">
+      <div v-if="b4" class="divider-bottom-4" />
+      <div v-else-if="b4flat" id="divider-bottom-4-flat" />
+    </div>
+    <div class="mt--2">
+      <div v-if="b5" class="divider-bottom-5" />
+      <div v-else class="invisible" />
+    </div>
+    <div class="fixed-width mt--2">
+      <div v-if="b6" class="divider-bottom-6" />
     </div>
   </div>
 </template>
 
 <style scoped>
-
 .container {
-  display: flex;
-  justify-content: center;
+  display: grid;
+  grid-template-rows: auto auto; /* Two rows */
+  grid-template-columns: 100px auto 100px 100px auto 100px;
 }
 
-#divider-top-row {
-  align-items: flex-start;
-  height: 100px;
+.fixed-width {
+  width: 100px; /* Fixed width for specified items */
 }
 
-#divider-top-1 {
+.divider-top-1 {
   height: 100px;
   background-size: 100px 100px;
-  background-image: url('~assets/images/corner-green-line-3-100px.svg');
+  background-image: url('@/assets/images/corner-green-line-3-100px.svg');
   background-repeat: no-repeat;
   flex: 0 0 100px;
 }
 
-#divider-top-2 {
+.divider-top-2 {
   border-bottom-color: var(--gc-green);
   border-bottom-width: 2px;
   border-bottom-style: solid;
 }
 
-#divider-top-3 {
+.divider-top-3 {
   height: 100px;
   background-size: 100px 100px;
-  background-image: url('~assets/images/corner-green-line-2-100px.svg');
+  background-image: url('@/assets/images/corner-green-line-2-100px.svg');
   background-repeat: no-repeat;
   flex: 0 0 100px;
   margin-right: -1px;
 }
 
-#divider-top-3-flat {
+.divider-top-3-flat {
   flex: 0 0 100px;
   border-bottom-color: var(--gc-green);
   border-bottom-width: 2px;
   border-bottom-style: solid;
 }
 
-#divider-top-4 {
+.divider-top-4 {
   height: 100px;
   background-size: 100px 100px;
-  background-image: url('~assets/images/corner-green-line-3-100px.svg');
+  background-image: url('@/assets/images/corner-green-line-3-100px.svg');
   background-repeat: no-repeat;
   flex: 0 0 100px;
   margin-left: -1px;
 }
 
-#divider-top-4-flat {
+.divider-top-4-flat {
   flex: 0 0 100px;
   border-bottom-color: var(--gc-green);
   border-bottom-width: 2px;
   border-bottom-style: solid;
 }
 
-#divider-top-5 {
+.divider-top-5 {
   border-bottom-color: var(--gc-green);
   border-bottom-width: 2px;
   border-bottom-style: solid;
 }
 
-#divider-top-6 {
+.divider-top-6 {
   height: 100px;
   background-size: 100px 100px;
-  background-image: url('~assets/images/corner-green-line-2-100px.svg');
+  background-image: url('@/assets/images/corner-green-line-2-100px.svg');
   background-repeat: no-repeat;
   flex: 0 0 100px;
 }
 
-#divider-bottom-row {
-  align-items: flex-end;
-  margin-top: -2px;
-  height: 100px;
-}
-
-#divider-bottom-1 {
+.divider-bottom-1 {
   height: 100px;
   background-size: 100px 100px;
-  background-image: url('~assets/images/corner-red-filled-4-100px.svg');
+  background-image: url('@/assets/images/corner-red-filled-4-100px.svg');
   background-repeat: no-repeat;
   flex: 0 0 100px;
 }
 
-#divider-bottom-2 {
+.divider-bottom-2 {
   border-top-color: var(--gc-green);
   border-top-width: 2px;
   border-top-style: solid;
 }
 
-#divider-bottom-3 {
+.divider-bottom-3 {
   height: 100px;
   background-size: 100px 100px;
-  background-image: url('~assets/images/corner-green-line-1-100px.svg');
+  background-image: url('@/assets/images/corner-green-line-1-100px.svg');
   background-repeat: no-repeat;
   flex: 0 0 100px;
   margin-right: -1px;
 }
 
-#divider-bottom-3-flat {
+.divider-bottom-3-flat {
   flex: 0 0 100px;
   border-top-color: var(--gc-green);
   border-top-width: 2px;
   border-top-style: solid;
 }
 
-#divider-bottom-4 {
+.divider-bottom-4 {
   height: 100px;
   background-size: 100px 100px;
-  background-image: url('~assets/images/corner-green-line-4-100px.svg');
+  background-image: url('@/assets/images/corner-green-line-4-100px.svg');
   background-repeat: no-repeat;
   flex: 0 0 100px;
   margin-right: -1px;
@@ -230,23 +237,21 @@ const props = defineProps({
   border-top-style: solid;
 }
 
-#divider-bottom-5 {
+.divider-bottom-5 {
   border-top-color: var(--gc-green);
   border-top-width: 2px;
   border-top-style: solid;
 }
 
-#divider-bottom-6 {
+.divider-bottom-6 {
   height: 100px;
   background-size: 100px 100px;
-  background-image: url('~assets/images/corner-green-line-1-100px.svg');
+  background-image: url('@/assets/images/corner-green-line-1-100px.svg');
   background-repeat: no-repeat;
   flex: 0 0 100px;
 }
 
-#divider-corner-invisible {
-  height: 100px;
-  background-size: 100px 100px;
-  flex: 0 0 100px;
+.invisible {
+  /* nothing to see here :) */
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -7,9 +7,9 @@ import Hero from '~/components/Hero.vue'
     <hero />
     <preview img="placeholder.png" content="about" />
     <!--preview value='about' img='about' link='/about' /-->
-    <!--divider b1 b2 t3 t4 t5 b5 b6 /-->
+    <divider b1 b2 t3 t4 b5 b6 />
     <!--contact /-->
-    <!--divider b1 b2 b3flat b4flat b5 t6 /-->
+    <!-- divider b1 b2 b3flat b4flat b5 t6 /-->
     <!--fwf-uni /-->
     <!--divider t1 t2 b3 /-->
   </div>
