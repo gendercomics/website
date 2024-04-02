b249e8726b8f15a68b71faaac7f99ae078e493c2
michael litschauer <michael.litschauer@gmail.com>
2024-04-02 13:04:04 +0200

    removed unnecessary style

--- components/TextPreviewBox.vue
+++ components/TextPreviewBox.vue
@@ -9,7 +9,7 @@ const props = defineProps({
 
 <template>
   <div class="container mt-10">
-    <div class="text-container w-90">
+    <div class="w-90">
       <p class="tag">ABOUT</p>
       <p class="titel-kachel">Zusammenfassung</p>
       <a class="a">Hallo Welt!</a>
