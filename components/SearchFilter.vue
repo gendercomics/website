4104803eca818fba1fbc83e8d31c3493a7e41eea
michael litschauer <michael.litschauer@gmail.com>
2024-04-09 10:22:01 +0200

    simplify active state

--- components/SearchFilter.vue
+++ components/SearchFilter.vue
@@ -1,48 +1,23 @@
 <script setup>
-const activeButtons = {
-  comics: true,
-  persons: false,
-  publisher: false,
-  keywords: false,
-}
-const filter = {
+const filter = reactive({
   comics: false,
   persons: false,
   publisher: false,
   keywords: false,
-}
+})
 </script>
 
 <template>
   <div class="flex container">
-    <bubble-button
-      label="Comics"
-      v-model:is-active="activeButtons.comics"
-      v-model:is-filter="filter.comics"
-      class="pr-1r"
-    />
-    <bubble-button
-      label="Persons"
-      v-model:is-active="activeButtons.persons"
-      v-model:is-filter="filter.persons"
-      class="pr-1r"
-    />
-    <bubble-button
-      label="Publisher"
-      v-model:is-active="activeButtons.publisher"
-      v-model:is-filter="filter.publisher"
-      class="pr-1r"
-    />
-    <bubble-button
-      label="Keywords"
-      v-model:is-active="activeButtons.keywords"
-      v-model:is-filter="filter.keywords"
-    />
+    <bubble-button label="Comics" v-model="filter.comics" class="pr-1r" />
+    <bubble-button label="Persons" v-model="filter.persons" class="pr-1r" />
+    <bubble-button label="Publisher" v-model="filter.publisher" class="pr-1r" />
+    <bubble-button label="Keywords" v-model="filter.keywords" />
   </div>
 </template>
 
 <style scoped>
 .container {
-  justify-content: center;;
+  justify-content: center;
 }
 </style>
