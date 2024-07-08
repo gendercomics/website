4ca818d5a0e24485834ce21439548e4be7626a1c
michael litschauer <michael.litschauer@gmail.com>
2024-07-08 12:29:40 +0200

    replaced w-90 by page-margin

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div class="pattern-background">
     <NavBar />
     <slot />
-    <div class="w-90">
+    <div class="page-margin">
       <divider t4 b5 b6 />
       <contact />
       <divider b1 b2 b3flat b4flat b5 t6 />
