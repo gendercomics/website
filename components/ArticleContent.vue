ef2677456cdbf86bb30e7c084c85404d3c71f0ab
michael litschauer <michael.litschauer@gmail.com>
2024-09-12 12:35:14 +0200

    rendering experiments

--- components/ArticleContent.vue
+++ components/ArticleContent.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import * as querystring from 'node:querystring'
 
 const props = defineProps({
   content: {
@@ -9,7 +8,7 @@ const props = defineProps({
 })
 const { locale } = useI18n()
 
-const i18nPath = '/' + locale.value + props.content
+const i18nPath = computed(() => '/' + locale.value + props.content)
 
 async function fetchContent() {
   try {
@@ -19,28 +18,44 @@ async function fetchContent() {
   }
 }
 
-const doc = await useAsyncData(() =>
-  queryContent('/' + locale.value + props.content).findOne(),
+// let { data: doc } = await useAsyncData(() => fetchContent(i18nPath.value))
+
+let doc = await useAsyncData(() =>
+  queryContent(i18nPath.value).findOne(),
 )
+
+// Watch the route and refetch content on route change
+/*
+watch(
+  () => route.fullPath,
+  async () => {
+    doc = await queryContent('/' + locale.value + props.content).findOne()
+  },
+)
+ */
+
+// Alternatively, using `onBeforeRouteUpdate`
+onBeforeRouteUpdate(async (to, from, next) => {
+  doc = await queryContent(i18nPath.value).findOne()
+  next()
+})
 </script>
 
 <template>
   <div class="container page-margin">
-    <client-only>
-      <content-renderer :value="doc" :key="$route.fullPath">
-        <div class="titel-xl mt-3rem txt-align-center">
-          {{ doc.data.value.title }}
+    <content-renderer :value="doc" :key="$route.fullPath">
+      <div class="titel-xl mt-3rem txt-align-center">
+        {{ doc.data.value.title }}
+      </div>
+      <div class="w-90">
+        <div v-if="doc.data.value.excerpt">
+          <content-renderer-markdown
+            class="a txt-align-center mt-2rem"
+            :value="doc.data.value.excerpt"
+          />
         </div>
-        <div class="w-90">
-          <div v-if="doc.data.value.excerpt">
-            <content-renderer-markdown
-              class="a txt-align-center mt-2rem"
-              :value="doc.data.value.excerpt"
-            />
-          </div>
-        </div>
-      </content-renderer>
-    </client-only>
+      </div>
+    </content-renderer>
 
     <divider-red-arrow />
     <img
@@ -52,24 +67,22 @@ const doc = await useAsyncData(() =>
 
     <div class="text-container">
       <div class="container-relative">
-        <client-only>
-          <content-renderer :value="doc" :key="$route.fullPath">
-            <div v-if="doc.data.value.image">
-              <article-image
-                :image="doc.data.value.image"
-                :caption="doc.data.value.caption"
-                class="image"
-              />
-            </div>
-            <h1>{{ doc.data.value.heading }}</h1>
-            <h2 v-if="doc.data.value.subheading">
-              {{ doc.data.value.subheading }}
-            </h2>
-            <div class="a mt-2rem">
-              <content-renderer-markdown :value="doc.data.value.body" />
-            </div>
-          </content-renderer>
-        </client-only>
+        <content-renderer :value="doc" :key="$route.fullPath">
+          <div v-if="doc.data.value.image">
+            <article-image
+              :image="doc.data.value.image"
+              :caption="doc.data.value.caption"
+              class="image"
+            />
+          </div>
+          <h1>{{ doc.data.value.heading }}</h1>
+          <h2 v-if="doc.data.value.subheading">
+            {{ doc.data.value.subheading }}
+          </h2>
+          <div class="a mt-2rem">
+            <content-renderer-markdown :value="doc.data.value.body" />
+          </div>
+        </content-renderer>
       </div>
     </div>
     <divider t1 b2 b3flat b4flat b5 t6 />
