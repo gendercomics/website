f6c2dc237aad1c1dc4954146a129027b45229af1
michael litschauer <michael.litschauer@gmail.com>
2024-10-07 13:06:54 +0200

    initial scaffold for comic page

---
+++ components/Comic.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const route = useRoute()
+
+const id = route.params.id
+console.log('ID= ' + id)
+</script>
+
+<template>
+  <div class="container page-margin">
+    <p>ID: {{ id }}</p>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ pages/comic/[id].vue
@@ -0,0 +1,17 @@
+<script setup>
+import Comic from '~/components/Comic.vue'
+
+const route = useRoute()
+const { locale } = useI18n()
+const slug = ref(route.path)
+
+onMounted(() => {
+  console.log('slug=' + slug.value)
+})
+</script>
+
+<template>
+  <main>
+    <comic :id="slug.value" :slug="slug.value" />
+  </main>
+</template>
