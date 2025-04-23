0593a561a02fff61cdcd6a31647c3ecea25add79
michael litschauer <michael.litschauer@gmail.com>
2025-04-23 14:14:51 +0200

    tracking in prod only

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,15 @@ import { useFeatureStore } from '~/stores/features.ts'
 import { init, trackPages } from 'insights-js'
 
 const featureStore = useFeatureStore()
-init('TrYgnSHvKAIkGdBl')
+const trackingDisabled = computed(() => {
+  const hostname = window.location.hostname
+  const isProductionDomain = hostname === 'gendercomics.net'
+  const isStaging =
+    hostname === 'stage.gendercomics.net' || hostname.includes('stage')
+  return !isProductionDomain || isStaging
+})
+
+init('TrYgnSHvKAIkGdBl', (disabled = trackingDisabled))
 trackPages()
 </script>
 
