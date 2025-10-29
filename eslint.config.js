1f94b7178ec56015b3e1618834b491d97c42b5a8
michael litschauer <michael.litschauer@gmail.com>
2025-10-29 12:27:23 +0100

    chore(conf): eslint conf

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
 import pluginVue from 'eslint-plugin-vue'
+import eslintConfigPrettier from 'eslint-config-prettier/flat'
 
 export default [
   // add more generic rulesets here, such as:
   // js.configs.recommended,
   ...pluginVue.configs['flat/recommended'],
+  eslintConfigPrettier,
   // ...pluginVue.configs['flat/vue2-recommended'], // Use this if you are using Vue.js 2.x.
   {
     rules: {
