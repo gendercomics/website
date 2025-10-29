b22ec729d9859c5a5df423d386c1d88b5edfebc8
michael litschauer <michael.litschauer@gmail.com>
2025-10-29 14:55:10 +0100

    chore!: migration to nuxt-content v3
    BREAKING-CHANGE: use new API for nuxt-content v3

--- components/TextPreviewBox.vue
+++ components/TextPreviewBox.vue
@@ -9,14 +9,13 @@ const { t } = useI18n({
   useScope: 'local',
 })
 const { locale } = useI18n()
-const route = useRoute()
 
 const i18nPath = computed(() => '/' + locale.value + props.content)
 const key = computed(() => 'preview:' + i18nPath.value)
 
-let doc = await useAsyncData(key.value, () =>
-  queryContent(i18nPath.value).findOne(),
-)
+const { data: doc } = await useAsyncData(key.value, () => {
+  return queryCollection('content').path(i18nPath.value).first()
+})
 
 onMounted(() => {
   console.log('TextPreviewBox key: ' + key.value)
@@ -26,15 +25,16 @@ onMounted(() => {
   <div class="container-row">
     <div class="column">
       <div class="w-90">
-        <content-renderer :value="doc" :key="key.value">
-          <div class="tag">{{ doc.data.value.tag }}</div>
-          <div class="titel-kachel">{{ doc.data.value.title }}</div>
-          <content-renderer-markdown
+        <div>
+          <div class="tag">{{ doc.tag }}</div>
+          <div class="titel-kachel">{{ doc.title }}</div>
+          <ContentRenderer
+            :key="doc.id"
+            :value="doc.excerpt"
             class="a mt-1rem"
-            :value="doc.data.value.excerpt"
-            :key="key.value"
           />
-        </content-renderer>
+        </div>
+
         <button-gray class="mt-1rem" :text="t('more')" :link="i18nPath" />
       </div>
     </div>
--- content.config.ts
+++ content.config.ts
@@ -1,10 +1,21 @@
+// website/content.config.ts
 import { defineCollection, defineContentConfig } from '@nuxt/content'
+import { z } from 'zod'
 
 export default defineContentConfig({
   collections: {
     content: defineCollection({
       type: 'page',
       source: '**/*.md',
+      schema: z.object({
+        excerpt: z
+          .object({
+            type: z.string(),
+            children: z.any(),
+          })
+          .optional(),
+        tag: z.string().optional(),
+      }),
     }),
   },
 })
